3305667e5b1f881b6315f483ccdd122ce91a33c4
MudassirAhmed0 <84226929+MudassirAhmed0@users.noreply.github.com>
2023-04-30 20:57:15 +0500

    oirign

--- vanilla/assets/js/home.js
+++ vanilla/assets/js/home.js
@@ -1,8 +1,10 @@
-const padding = (window.innerWidth / 100) * 8.4375;
-const margin = (window.innerWidth / 100) * 1.19791666667;
+let isDesktop = window.innerWidth > 768;
+let padding = isDesktop && (window.innerWidth / 100) * 8.4375;
+let margin = isDesktop ? (window.innerWidth / 100) * 1.19791666667 : 15;
 const owl = $(".owl-carousel").owlCarousel({
-  // loop: true,
+  loop: !isDesktop,
   nav: true,
+  center: !isDesktop,
   dots: false,
   margin: margin,
   stagePadding: padding,
@@ -10,7 +12,7 @@ const owl = $(".owl-carousel").owlCarousel({
 
   responsive: {
     0: {
-      items: 1,
+      items: 1.55,
     },
     600: {
       items: 3,
@@ -24,56 +26,65 @@ const owl = $(".owl-carousel").owlCarousel({
 $(".owl-carousel__next").click(() => owl.trigger("next.owl.carousel"));
 $(".owl-carousel__prev").click(() => owl.trigger("prev.owl.carousel"));
 
-let isNext = true;
-let isPrev = false;
+if (isDesktop) {
+  let isNext = true;
+  let isPrev = false;
 
-owl.on("changed.owl.carousel", function (event) {
-  if ($(".owl-next").hasClass("disabled")) {
-    isNext = false;
-    meraRight.classList.add("opacity-0");
-    meraRight.classList.add("pointer-events-none");
-  } else {
-    isNext = true;
-  }
-  if ($(".owl-prev").hasClass("disabled")) {
-    isPrev = false;
-    meraLeft.classList.add("opacity-0");
-    meraLeft.classList.add("pointer-events-none");
-  } else {
-    isPrev = true;
-  }
-});
-
-const meraLeft = document.querySelector(".meraLeft");
-const meraRight = document.querySelector(".meraRight");
-const carouselWrapper = document.querySelector(".carousel-wrapper");
-
-carouselWrapper.addEventListener("mousemove", (e) => {
-  if ((window.innerWidth - padding) / 2 > e.clientX) {
-    if (!isPrev) {
-      meraLeft.classList.add("opacity-0");
-      meraLeft.classList.add("pointer-events-none");
-    } else {
-      meraLeft.classList.remove("opacity-0");
-      meraLeft.classList.remove("pointer-events-none");
-    }
-    meraRight.classList.add("opacity-0");
-    meraRight.classList.add("pointer-events-none");
-  } else {
-    meraLeft.classList.add("opacity-0");
-    meraLeft.classList.add("pointer-events-none");
-    if (!isNext) {
+  owl.on("changed.owl.carousel", function (event) {
+    if ($(".owl-next").hasClass("disabled")) {
+      isNext = false;
       meraRight.classList.add("opacity-0");
       meraRight.classList.add("pointer-events-none");
     } else {
-      meraRight.classList.remove("opacity-0");
-      meraRight.classList.remove("pointer-events-none");
+      isNext = true;
     }
-  }
-});
-carouselWrapper.addEventListener("mouseout", () => {
-  meraLeft.classList.add("opacity-0");
-  meraLeft.classList.add("pointer-events-none");
-  meraRight.classList.add("opacity-0");
-  meraRight.classList.add("pointer-events-none");
+    if ($(".owl-prev").hasClass("disabled")) {
+      isPrev = false;
+      meraLeft.classList.add("opacity-0");
+      meraLeft.classList.add("pointer-events-none");
+    } else {
+      isPrev = true;
+    }
+  });
+
+  const meraLeft = document.querySelector(".meraLeft");
+  const meraRight = document.querySelector(".meraRight");
+  const carouselWrapper = document.querySelector(".carousel-wrapper");
+
+  carouselWrapper.addEventListener("mousemove", (e) => {
+    if ((window.innerWidth - padding) / 2 > e.clientX) {
+      if (!isPrev) {
+        meraLeft.classList.add("opacity-0");
+        meraLeft.classList.add("pointer-events-none");
+      } else {
+        meraLeft.classList.remove("opacity-0");
+        meraLeft.classList.remove("pointer-events-none");
+      }
+      meraRight.classList.add("opacity-0");
+      meraRight.classList.add("pointer-events-none");
+    } else {
+      meraLeft.classList.add("opacity-0");
+      meraLeft.classList.add("pointer-events-none");
+      if (!isNext) {
+        meraRight.classList.add("opacity-0");
+        meraRight.classList.add("pointer-events-none");
+      } else {
+        meraRight.classList.remove("opacity-0");
+        meraRight.classList.remove("pointer-events-none");
+      }
+    }
+  });
+  carouselWrapper.addEventListener("mouseout", () => {
+    meraLeft.classList.add("opacity-0");
+    meraLeft.classList.add("pointer-events-none");
+    meraRight.classList.add("opacity-0");
+    meraRight.classList.add("pointer-events-none");
+  });
+}
+
+window.addEventListener("resize", () => {
+  isDesktop = window.innerWidth > 768;
+  padding = isDesktop && (window.innerWidth / 100) * 8.4375;
+  margin = isDesktop ? (window.innerWidth / 100) * 1.19791666667 : 15;
+  owl.onResize();
 });
